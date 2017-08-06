fb6714444f539570f0939d11010e79050a650090
Jose Enrique Marquez <j.marquez@nuevahospitality.com>
2017-08-06 13:47:27 -0400

    Add drawer in header menu

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
+// Dependencies
 import React, { Component } from 'react';
+
+// Dependencies Material
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
 import logo from './logo.svg';
 import './App.css';
 
+injectTapEventPlugin();
+
 class App extends Component {
+  state = {
+    open: false
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+  handleClose = () => this.setState({open: false});
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AppBar
+          title="Title"
+          onLeftIconButtonTouchTap={this.handleToggle}
+          />
+        <Drawer docked={false} open={this.state.open} onRequestChange={(open) => this.setState({open})}>
+          <MenuItem>Menu Item</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+        </Drawer>
+
       </div>
     );
   }
