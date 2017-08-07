2359bc239e6fc7bbcdfb56c3a426fe5284a9515d
Jose Enrique Marquez <j.marquez@nuevahospitality.com>
2017-08-07 08:08:28 -0400

    Add component not found

--- src/App.js
+++ src/App.js
@@ -9,6 +9,19 @@ import fusTheme from './fusTheme';
 
 // Components
 import SideNav from './SideNav';
+/* Import Views */
+import Home from './views/Home'
+import LogosPosters from './views/LogosPosters'
+import Logos from './views/Logos'
+import Posters from './views/Posters'
+import Letterhead from './views/Letterhead'
+import Story from './views/Story'
+import PlanningGuide from './views/PlanningGuide'
+import Services from './views/Services'
+import Glossary from './views/Glossary'
+import ServiceRequest from './views/ServiceRequest'
+import Tutorial from './views/Tutorial'
+import NotFound from './views/NotFound'
 
 // Styles
 import './App.css';
@@ -24,13 +37,18 @@ class App extends Component {
             
             <SideNav />
 
-            <Route exact path="/" render={() => <h1>Home view</h1>} />
-            <Route exact path="/logos" render={() => <h1>logos view</h1>} />
-            <Route exact path="/posters" render={() => <h1>posters view</h1>} />
-            <Route exact path="/letterhead" render={() => <h1>letterhead view</h1>} />
-            <Route exact path="/share-a-story" render={() => <h1>Share a Story view</h1>} />
-            <Route exact path="/service-request-form" render={() => <h1>Service Request view</h1>} />
-            <Route exact path="/tutorial" render={() => <h1>Tutorial view</h1>} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/logos-poster" component={LogosPosters} />
+            <Route exact path="/logos" component={Logos} />
+            <Route exact path="/posters" component={Posters}/>
+            <Route exact path="/letterhead" component={Letterhead} />
+            <Route exact path="/share-a-story" component={Story} />
+            <Route exact path="/planning-guide" component={PlanningGuide} />
+            <Route exact path="/services" component={Services} />
+            <Route exact path="/glossary" component={Glossary} />
+            <Route exact path="/service-request-form" component={ServiceRequest} />
+            <Route exact path="/tutorial" component={Tutorial} />
+            <Route component={NotFound} />
 
           </div>
         </MuiThemeProvider>
