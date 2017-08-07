0389d10fa0ae3ddcaaf57ab6ffd333f010579147
Jose Enrique Marquez <j.marquez@nuevahospitality.com>
2017-08-07 07:58:53 -0400

    Refactoring

--- src/App.js
+++ src/App.js
@@ -2,60 +2,27 @@
 import React, { Component } from 'react';
 
 // Dependencies Material
-import { BrowserRouter, Route, NavLink } from 'react-router-dom';
-
-import AppBar from 'material-ui/AppBar';
-import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
-
+import { BrowserRouter, Route } from 'react-router-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import {MuiThemeProvider} from 'material-ui/styles/';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {MuiThemeProvider, getMuiTheme} from 'material-ui/styles';
 import fusTheme from './fusTheme';
 
-import logo from './logo.svg';
+// Components
+import SideNav from './SideNav';
+
+// Styles
 import './App.css';
 
 injectTapEventPlugin();
 
-
-
 class App extends Component {
-  state = {
-    open: false
-  }
-
-  handleToggle = () => this.setState({open: !this.state.open});
-  handleClose = () => this.setState({open: false});
-
-  SideBarItem = ({ link, text, isExact }) => {
-    return (
-      <NavLink activeClassName="active" exact={isExact} to={link} >
-        <MenuItem onTouchTap={this.handleClose} primaryText={text} />
-      </NavLink>
-    );
-  }
-
   render() {
-    const SideBarItem = this.SideBarItem;
     return (
       <BrowserRouter>
         <MuiThemeProvider muiTheme={getMuiTheme(fusTheme)}>
           <div>
-            <AppBar
-              title="Title"
-              onLeftIconButtonTouchTap={this.handleToggle}
-              />
-
-            <Drawer docked={false} open={this.state.open} onRequestChange={(open) => this.setState({open})}>
-              <SideBarItem link="/" text="Home" isExact />
-              <SideBarItem link="/logos" text="Logos" />
-              <SideBarItem link="/posters" text="Posters" />
-              <SideBarItem link="/letterhead" text="Letterhead" />
-              <SideBarItem link="/share-a-story" text="Share a Story" />
-              <SideBarItem link="/service-request-form" text="Service Request Form" />
-              <SideBarItem link="/tutorial" text="Tutorial" />
-            </Drawer>
+            
+            <SideNav />
 
             <Route exact path="/" render={() => <h1>Home view</h1>} />
             <Route exact path="/logos" render={() => <h1>logos view</h1>} />
---
+++ src/views/Services.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Service extends Component {
+  render() {
+    return <h1>Service View</h1>
+  }
+}
+
+export default Service
