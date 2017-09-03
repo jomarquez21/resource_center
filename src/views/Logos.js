d7478c7e844568698ff33817f6df9ad8c1f9e76b
Jose Enrique Marquez <j.marquez@nuevahospitality.com>
2017-09-03 19:28:52 -0400

    Generic Card

--- src/views/Logos.js
+++ src/views/Logos.js
@@ -1,8 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import GenericCard from './../components/GenericCard'
+import { CardTitle } from 'material-ui/Card'
 
 class Logos extends Component {
   render() {
-    return <h1>Logos View</h1>
+    return (
+      <div className="row">
+        <div className="col m6">
+          <GenericCard
+            headerTitle="Hello"
+            headerSubtitle="I am Jose Marquez"
+            headerAvatar="http://via.placeholder.com/100x200"
+            mediaImgSrc="http://via.placeholder.com/120x100"
+            overlay={
+              <CardTitle
+                title="Nice job"
+                subtitle="Congratulations on this great Achievement"
+              />
+            }
+            cardTitle="Nice one"
+            cardSubtitle="looking good"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </GenericCard>
+        </div>
+      </div>
+    )
   }
 }
 
